56f1f0c255d80f21d55828ba0c7d40bdeb62431b
Nestor Pina <nestor.pina@gmail.com>
2013-03-20 17:46:56 +0100

    Change POST of angular to send values in FormData (for jersey to read)
    By default it sends parameters as Request Payload, and jersey ignores
    them.
    Read :
    http://victorblog.com/2012/12/20/make-angularjs-http-service-behave-like-jquery-ajax/

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,7 +2,15 @@
 
 /* App Module */
 
-angular.module('nestorshop', ['nestorshopServices']).
+angular.module('nestorshop', ['nestorshopServices'], function($httpProvider) {
+	  // Use x-www-form-urlencoded Content-Type
+	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+	 
+	  // Override $http service's default transformRequest
+	$httpProvider.defaults.transformRequest = [function(data) {
+	    return angular.isObject(data) && String(data) !== '[object File]' ? jQuery.param(data) : data;
+	  }];	
+  }).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/products', {templateUrl: 'partials/product-list.html',   controller: ProductListCtrl}).
@@ -10,4 +18,6 @@ angular.module('nestorshop', ['nestorshopServices']).
       when('/shoplists', {templateUrl: 'partials/shoplist-list.html',   controller: ShopListsCtrl}).
       when('/shoplists/:shoplistId', {templateUrl: 'partials/shoplist-detail.html',   controller: ShopListDetailCtrl}).
       otherwise({redirectTo: '/products'});
+
 }]);
+
